4cf84b7783d11d4306086ef2ea2b53ab1b2e1eae
fletcher <fletcher@localhost.localdomain>
2020-03-11 20:54:07 -0700

    added sidedrawer,navigation and toolbar

---
+++ src/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -0,0 +1,19 @@
+import React from "react";
+import classes from "./NavigationItem.module.css";
+//import {NavLink} from 'react-router-dom';
+
+const navigationItem = props => {
+  return (
+    <li className={classes.NavigationItem}>
+      <a href="/" className={props.active ? classes.active : undefined}>
+        {props.children}
+      </a>
+      {/* <NavLink 
+                to={props.link}
+                activeClassName={classes.active}
+                exact={props.exact}></NavLink> */}
+    </li>
+  );
+};
+
+export default navigationItem;
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,13 @@
 import React from "react";
 import Aux from "../../hoc/Aux";
 import classes from "./Layout.module.css";
+import Toolbar from "../../Navigation/Toolbar/Toolbar";
+import SideDrawer from "../../Navigation/Sidedrawer/Sidedrawer";
 
 const layout = props => (
   <Aux>
-    <div>Toolbar, Sidedrawer, Backdrop</div>
+    <Toolbar></Toolbar>
+    <SideDrawer></SideDrawer>
     <main className={classes.Content}>{props.children}</main>
   </Aux>
 );
